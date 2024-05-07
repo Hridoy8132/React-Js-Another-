9f1c8830cc249624603dddc4f45ebcd1f204ada4
Md. Nurul Haider Hridoy <hridoy0182726@gmail.com>
2024-05-07 17:58:51 +0600

    Lec-8 (Props)

--- src/App.js
+++ src/App.js
@@ -5,14 +5,10 @@ import Card from './components/Card';
 function App(){
    return <div>
             <h1 className='headingStyle'> My Application </h1>
-            <Card />   
-            <Card />
-            <Card />
- 
-
-
-         {/* //This is a card component using for display multiple times. */}
-
+            <Card titleText= "Call Mother" descText= " This is the desc-1"/> 
+            <Card titleText= "Call Father" descText= " This is the desc-2"/>  
+            <Card titleText= "Call Brother" descText= " This is the desc-3"/>    
+            
 
         </div>
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
 // Anis: Lec-5-7 start (React Function Component)
-const todoTitle = "Call Mother";
-const todoDesc = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, distinctio!";
+// const todoTitle = "Call Mother";
+// const todoDesc = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, distinctio!";
+
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const currentYear = date.getFullYear();
 
 
-function Card(){
+function Card(props){
+  console.log(props)
   return <div className='card'>
-            <h3 className='cardTitle'> {todoTitle}</h3>
-            <p  className='cardDesc'>  {todoDesc}</p>
+            <h3 className='cardTitle'> {props.titleText}</h3>
+            <p  className='cardDesc'>  {props.descText}</p>
             <p className='cardFooter'>  {dateName + "/" + monthName + "/" + currentYear  }</p>
 
         </div> 
